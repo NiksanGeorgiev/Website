ff9511a2d73283b9d5b67946ff2c884e0847dea4
NikolayGerogiev <nikolaypavgr@gmail.com>
2021-04-26 00:30:38 +0100

    SUPER MEGA GENIALEN SUM BE MAMKA MU

--- Website/javascript/include.js
+++ Website/javascript/include.js
@@ -11,8 +11,8 @@ function include() {
         if (this.readyState == 4) {
           if (this.status == 200) {elmnt.innerHTML = this.responseText;}
           if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          elmnt.removeAttribute("w3-include-html");
-          includeHTML();
+          elmnt.removeAttribute("include");
+          include();
         }
       }
       xhttp.open("GET", file, true);
